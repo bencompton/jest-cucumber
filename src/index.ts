fec536e5a6c1845c24220bbd18b0cce1abb1d8d1
Ben Compton <benjamincompton@gmail.com>
2018-10-05 09:19:39 -0700

    Add parseFeature to public API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { loadFeature } from './parsed-feature-loading';
+export { loadFeature, parseFeature } from './parsed-feature-loading';
 export { defineFeature, DefineStepFunction } from './feature-definition-creation';
 export { setJestCucumberConfiguration } from './configuration';
 export { generateCodeFromFeatureByLineNumber } from './code-generation/generate-code-from-feature-line';
