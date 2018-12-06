7228112901d86f2648832a5af19e0ac46773af49
Nathan Russell <nathan.d.russell@hotmail.com>
2018-12-06 08:44:30 -0500

    Added support for the AND keyword.

--- src/code-generation/scenario-generation.ts
+++ src/code-generation/scenario-generation.ts
@@ -3,7 +3,7 @@ import { generateStepCode, generateStepFunctionCall } from './step-generation';
 import { indent } from './utils';
 
 const scenarioTemplate = (scenarioTitle: string, steps: string) => {
-    return `test('${scenarioTitle}', ({ given, when, then }) => {\n${indent(steps, 1).slice(0, -1)}\n});`;
+    return `test('${scenarioTitle}', ({ given, when, then, and }) => {\n${indent(steps, 1).slice(0, -1)}\n});`;
 };
 
 export const generateScenarioCode = (scenario: ParsedScenario | ParsedScenarioOutline) => {
